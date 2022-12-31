2b8e4ad37607ad880c549d38662cf4021f0e9d5a
John Danielsson <JohnDanielsson@JDs-MacBook-Pro-work.local>
2022-12-31 10:40:44 -0800

    split into train/test sets

--- linear_regression.py
+++ linear_regression.py
@@ -1,8 +1,11 @@
 # Import libraries
 import pandas as pd
+from sklearn.linear_model import LinearRegression
 import numpy as np
 from math import sqrt
-from sklearn.metrics import mean_absolute_error as mae
+from sklearn.metrics import r2_score
+from sklearn.metrics import mean_squared_error as mse
+from sklearn.model_selection import train_test_split
 from statsmodels.formula.api import ols
 from scipy.stats import pearsonr
 import seaborn as sns
@@ -13,8 +16,6 @@ from matplotlib.patches import Patch
 # Make sure 'World_Energy_Consumption.csv' is in the directory
 df = pd.read_csv('World_Energy_Consumption.csv')
 columns = [
-    'country',
-    'year',
     'population',
     'gdp',
     'energy_per_capita',
@@ -24,66 +25,52 @@ columns = [
 # Feature Engineering / Data Wrangling
 df = df[columns].dropna()
 df['gdp_per_capita'] = df['gdp'] / df['population']
+# Remove outliers
+gdp_top = df['gdp_per_capita'].quantile(0.95)
+gdp_bottom = df['gdp_per_capita'].quantile(0.05)
+energy_top = df['energy_per_capita'].quantile(0.95)
+energy_bottom = df['energy_per_capita'].quantile(0.05)
+df = df[(gdp_bottom < df['gdp_per_capita']) & (df['gdp_per_capita'] < gdp_top)]
+df = df[(energy_bottom < df['energy_per_capita']) & (df['energy_per_capita'] < energy_top)]
+
 # Average GDP per capita divided by average energy use per capita
 baseline = np.average(df['gdp_per_capita'])
 baseline /= np.average(df['energy_per_capita'])
 df['baseline'] = range(df.shape[0]) * baseline
 
-# Linear Regression
-# Dependent variable
+# Make independent (X) and dependent (y) variables
+X = np.array(df['energy_per_capita'])[:, np.newaxis]
 y = df['gdp_per_capita']
-# Independent variable
-x = df['energy_per_capita']
-# Linear regression model (Ordinary Least Squares)
-lr = ols('y ~ x', data=df).fit()
-params = lr.params
-intercept = params[0]
-slope = params[1]
-print('LINE OF BEST FIT')
-print(f'            baseline slope = {baseline}')
-print(f'    line-of-best-fit slope = {slope}')
-print(f'line-of-best-fit intercept = {intercept}')
-print()
 
-# Compare predictive power of baseline and line-of-best-fit prediction
-r2_x_y = pearsonr(x=x, y=y)
-r2_baseline_y = pearsonr(x=x, y=df['baseline'])
-print('CORRELATION')
-print('Correlation for GDP per capita (y) and energy per capita (x)')
-print(f'Pearson\'s correlation coefficient = {r2_x_y[0]}')
-print('Correlation for baseline (y) and energy per capita (x)')
-print(f'Pearson\'s correlation coefficient = {r2_baseline_y[0]}')
-print()
+# Split data into training and testing sets.
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=123)
+# print(X_train.shape)
 
-# Add y_pred column to df
-df['y_pred'] = df['energy_per_capita'] * slope + intercept
+# Set up Linear Regression
+model = LinearRegression()
+model.fit(X=X_train, y=y_train)
+slope = model.coef_
+intercept = model.intercept_
+print(f'Predicted GDP per capita = (Energy use per capita)*{slope} + {intercept}')
 
-# Calculate evaluation metrics for baseline and line-of-best-fit
-rmse_baseline = sqrt(mae(df['baseline'], df['gdp_per_capita']))
-rmse_lobf = sqrt(mae(df['y_pred'], df['gdp_per_capita']))
-print('EVALUATION METRICS')
-print(f'        RMSE for baseline = {rmse_baseline}')
-print(f'RMSE for line-of-best-fit = {rmse_lobf}')
-print()
+# Predict GDP per capita
+y_pred = model.predict(X_test)
+print(f'      LR r^2 = {r2_score(y_test, y_pred)}')
+baseline_predict = [baseline*x for x in y_pred]
+print(f'baseline r^2 = {r2_score(baseline_predict, y_pred)}')
+print(f'        rmse = {sqrt(mse(y_test, y_pred))}')
 
-# Data visualization
-grid = sns.JointGrid(x=x, y=y, space=0, height=6, ratio=50)
-grid.plot_joint(plt.scatter, color="g")
-plt.plot(
-    [0, 3e5],
-    [0, 3e5*baseline],
-    linewidth=2,
-    label='baseline',
-    color='blue'
+# Data Visualization
+sns.regplot(x=X_train, y=y_train, line_kws={"color": "red"})
+energy_range = range(int(max(df['energy_per_capita'])))
+sns.lineplot(
+    x=energy_range,
+    y=[int(baseline*i) for i in energy_range],
+    color='black'
 )
-plt.plot(
-    [0, 3e5],
-    [intercept, intercept+3e5*slope],
-    linewidth=2,
-    label='regression',
-    color='red'
-)
-blue_patch = Patch(color='blue', label='baseline')
+plt.xlabel('Energy use per capita')
+plt.ylabel('GDP per capita')
+black_patch = Patch(color='black', label='baseline')
 red_patch = Patch(color='red', label='regression')
-plt.legend(handles=[blue_patch, red_patch], loc=(0.02, 0.6))
+plt.legend(handles=[black_patch, red_patch], loc=(0.02, 0.8))
 plt.show()
